09b33b486048d1b1d78fe6375402a0b4ada6f689
Seungtae Kim <kst20209@naver.com>
2021-02-12 01:40:25 +0900

    Fixed: 날짜가 잘못 계산되는 문제 해결

--- src/App.js
+++ src/App.js
@@ -21,8 +21,10 @@ class App extends Component{
   dateDiff(_date1, _date2){
     var diffDate_1 = _date1 instanceof Date ? _date1 :new Date(_date1);
     var diffDate_2 = _date2 instanceof Date ? _date2 :new Date(_date2);
-    diffDate_1 =new Date(diffDate_1.getFullYear(), diffDate_1.getMonth()+1, diffDate_1.getDate());
-    diffDate_2 =new Date(diffDate_2.getFullYear(), diffDate_2.getMonth()+1, diffDate_2.getDate());
+    diffDate_1 =new Date(diffDate_1.getFullYear(), diffDate_1.getMonth(), diffDate_1.getDate());
+    diffDate_2 =new Date(diffDate_2.getFullYear(), diffDate_2.getMonth(), diffDate_2.getDate());
+    console.log(diffDate_1);
+    console.log(diffDate_2);
     var diff = Math.abs(diffDate_2.getTime() - diffDate_1.getTime());
     diff = Math.ceil(diff / (1000 * 3600 * 24));
     return diff; 
