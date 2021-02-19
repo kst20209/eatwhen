418fcaeaf3f70b0d221aae02c256f33e4c0f72d1
Seungtae Kim <kst20209@naver.com>
2021-02-19 15:16:13 +0900

    Improved: progressbar css 적용 확인

--- src/components/Read.js
+++ src/components/Read.js
@@ -6,30 +6,31 @@ class Read extends Component {
       var date = this.props.date;
       var std = this.props.std;
       var number = (1-(date/std))*100;
-      var color = "#A0D468";
+      //var color = "#A0D468";
+      var color = "success"
       var level = "정상";
       if (number < 0){
           number = 0;
           level = "비상";
       }
       if (33 <= number && number < 66){
-        color= "#FFE650";
+        //color= "#FFE650";
+        color = "warning";
         level = "경고";
       } else if (number < 33) {
-        color = "#FF013D";
+        //color = "#FF013D";
+        color = "danger";
         level = "위험";
       }
-      
-      if (!color){
-        alert('Hello!');
-      }
+
+      number = Math.round(number);
 
       return(
         <div class='read'>
             {this.props.img}
             {this.props.title}
             <span>현재 {this.props.title} 수치: {level}</span>
-            <ProgressBar now={number} />
+            <ProgressBar class="progress" now={number} label={`${number}%`} variant={color}/>
             <span>마지막으로 먹은게 {date}일 전...</span>
         </div>
       );
